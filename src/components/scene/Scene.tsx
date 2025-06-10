ee989f65903be4c477fda238e6d551f5c8eb2639
irkan <irkan.ehmedov@gmail.com>
2025-06-10 14:27:00 +0400

    Refactor Scene component by simplifying lighting setup and removing unnecessary children rendering. Update PerspectiveCamera configuration for improved clarity in the scene structure.

--- src/components/scene/Scene.tsx
+++ src/components/scene/Scene.tsx
@@ -40,19 +40,11 @@ const Scene = ({ children, aylaModelRef }: SceneProps) => {
       <div className="canvas-container" ref={canvasContainerRef} style={{ width: '100%', height: '100%', position: 'relative' }}>
         <Canvas style={{ background: 'transparent' }}>
           <Suspense fallback={null}>
-            <PerspectiveCamera makeDefault position={[0, 0, 1]} fov={50} />
+            <PerspectiveCamera makeDefault position={[0, 0, 1]} />
             <OrbitControls target={[0, 0, 0]} />
-
             <ambientLight intensity={0.5} />
-            <directionalLight position={[10, 10, 5]} intensity={1} />
-            <pointLight position={[-10, -10, -10]} intensity={0.5} />
-
-            {/* Render children here */}
-            {children}
-
-            {/* Ayla character model */}
+            <directionalLight position={[10, 10, 5]} />
             <Model position={[0, -1.4, 0]} ref={aylaModelRef} />
-
             <Environment preset="sunset" />
           </Suspense>
         </Canvas>
